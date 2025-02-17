5f5c987d161cd84aee628cb13b77810ffa3ee970
Igor Mytyuk <igor.mytyuk@gmail.com>
2025-02-17 09:35:58 +0100

    fix: Resolve JSX issues by adding improt source in tsconfig.json

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -6,7 +6,7 @@ import SocialLinks from './SocialLinks.vue';
     <header class="relative z-50 text-gray-900">
         <div class="flex items-center px-4 py-6 mx-auto sm:px-6 space-x-4">
             <div class="flex items-center justify-start flex-1">
-                <CodeBracketIcon class="mr-2 h-4 w-4" aria-hidden="true" />
+                <CodeBracketIcon class="mr-2 h-4 w-4" />
                 <h2 class="transform-gpu text-sm font-bold">Igor Mytyuk</h2>
             </div>
             <SocialLinks />
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -27,30 +27,30 @@ function mailto() {
           class="py-3 px-6 md:px-12 inline-flex items-center rounded-md bg-black font-medium text-white shadow-sm"
           type="button">
           Resume
-          <ArrowDownTrayIcon class="-mr-1 ml-2 h-5 w-5" aria-hidden="true" />
+          <ArrowDownTrayIcon class="-mr-1 ml-2 h-5 w-5" />
         </a>
         <button @click="mailto"
           class="py-3 px-6 md:px-12 inline-flex items-center rounded-md border bg-white font-medium border-black-500 text-black-600"
           type="button">
           Contact Me
-          <PaperAirplaneIcon class="-mr-1 ml-2 h-5 w-5" aria-hidden="true" />
+          <PaperAirplaneIcon class="-mr-1 ml-2 h-5 w-5" />
         </button>
       </div>
 
 
       <div class="flex flex-col md:flex-row md:space-x-4 md:space-y space-y-4 space-x mt-8">
         <div class="flex-1">
-          <CodeBracketIcon class="mr-2 h-8 w-8 mb-2" aria-hidden="true" />
+          <CodeBracketIcon class="mr-2 h-8 w-8 mb-2" />
           <h2 class="text-xl font-bold">Full Stack</h2>
           <p class="w-3/4">Full stack developer with a focus on Front-end development.</p>
         </div>
         <div class="flex-1">
-          <PaintBrushIcon class="mr-2 h-8 w-8 mb-2" aria-hidden="true" />
+          <PaintBrushIcon class="mr-2 h-8 w-8 mb-2" />
           <h2 class="text-xl font-bold">UI/UX</h2>
           <p class="w-3/4">I'm just in love in creating fancy user interfaces.</p>
         </div>
         <div class="flex-1">
-          <DevicePhoneMobileIcon class="mr-2 h-8 w-8 mb-2" aria-hidden="true" />
+          <DevicePhoneMobileIcon class="mr-2 h-8 w-8 mb-2" />
           <h2 class="text-xl font-bold">Mobile</h2>
           <p class="w-3/4">Yes, I develop even mobile applications, but for now only Android apps.</p>
         </div>
